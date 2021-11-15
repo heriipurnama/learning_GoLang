3c0d3410b3a26f7949833d4acf3126628c9630e8
heripurnama <heri.purnama@xapiens.id>
2021-11-15 11:29:19 +0700

    function return value

--- day_3_function/function.go
+++ day_3_function/function.go
@@ -7,10 +7,28 @@ func sayHelo1() {
 	fmt.Println("hello")
 }
 
+func sayHeloTo(firstName string, lastName string) {
+	fmt.Println("hello", firstName, lastName)
+}
+
+// function return value
+func getHello(name string) string {
+	return "Hello" + name
+}
+
 func main() {
 	fmt.Println("========function=======")
 	for i := 0; i < 10; i++ {
 		sayHelo1()
 	}
+	sayHeloTo("'heriipurnama'", "'reina'")
+	// bisa langsung! //function parameter
+	fmt.Println("=======function parameter====")
+	firstName := "sofy"
+	sayHeloTo(firstName, "purnama")
+	sayHeloTo("reina", "sofiana")
 
+	// function return value
+	result := getHello("reina")
+	fmt.Println("result: ", result)
 }
